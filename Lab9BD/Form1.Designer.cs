50216e9f170f53a708387c3125d1f03982b8a751
iraki <iraki@PCIL>
2024-05-21 00:18:52 +0300

    Добавьте файлы проекта.

---
+++ Lab9BD/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace Lab9BD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ColumnBox = new System.Windows.Forms.TextBox();
+            this.ForeignBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.TableCreateBox = new System.Windows.Forms.TextBox();
+            this.CreateTable = new System.Windows.Forms.Button();
+            this.DeleteTable = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.ListingLable = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Создать БД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Название создаваемой БД/\r\nИспользуемой";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Название таблицы\r\nдля изменения";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(24, 52);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(123, 20);
+            this.NameBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Название столбца";
+            // 
+            // ColumnBox
+            // 
+            this.ColumnBox.Location = new System.Drawing.Point(24, 111);
+            this.ColumnBox.Name = "ColumnBox";
+            this.ColumnBox.Size = new System.Drawing.Size(123, 20);
+            this.ColumnBox.TabIndex = 6;
+            // 
+            // ForeignBox
+            // 
+            this.ForeignBox.Location = new System.Drawing.Point(387, 53);
+            this.ForeignBox.Name = "ForeignBox";
+            this.ForeignBox.Size = new System.Drawing.Size(140, 20);
+            this.ForeignBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Тип данных SQL";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 47);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 47);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(19, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 48);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Сохранить листинг в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // TableCreateBox
+            // 
+            this.TableCreateBox.Location = new System.Drawing.Point(6, 45);
+            this.TableCreateBox.Name = "TableCreateBox";
+            this.TableCreateBox.Size = new System.Drawing.Size(132, 20);
+            this.TableCreateBox.TabIndex = 13;
+            // 
+            // CreateTable
+            // 
+            this.CreateTable.Location = new System.Drawing.Point(6, 74);
+            this.CreateTable.Name = "CreateTable";
+            this.CreateTable.Size = new System.Drawing.Size(132, 23);
+            this.CreateTable.TabIndex = 14;
+            this.CreateTable.Text = "Создать таблицу";
+            this.CreateTable.UseVisualStyleBackColor = true;
+            this.CreateTable.Click += new System.EventHandler(this.CreateTable_Click);
+            // 
+            // DeleteTable
+            // 
+            this.DeleteTable.Location = new System.Drawing.Point(6, 108);
+            this.DeleteTable.Name = "DeleteTable";
+            this.DeleteTable.Size = new System.Drawing.Size(132, 23);
+            this.DeleteTable.TabIndex = 15;
+            this.DeleteTable.Text = "Удалить таблицу";
+            this.DeleteTable.UseVisualStyleBackColor = true;
+            this.DeleteTable.Click += new System.EventHandler(this.DeleteTable_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Название таблицы";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(592, 302);
+            this.tabControl1.TabIndex = 17;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(584, 276);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Создание/удаление БД";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.DeleteTable);
+            this.tabPage2.Controls.Add(this.TableCreateBox);
+            this.tabPage2.Controls.Add(this.CreateTable);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(584, 276);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Работа с таблицами в БД";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.button5);
+            this.tabPage3.Controls.Add(this.comboBox1);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.ColumnBox);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.NameBox);
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.ForeignBox);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(584, 276);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Работа со столбцами в таблице";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.ListingLable);
+            this.tabPage4.Controls.Add(this.button4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(584, 276);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Сохранение результатов";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // ListingLable
+            // 
+            this.ListingLable.AutoSize = true;
+            this.ListingLable.Location = new System.Drawing.Point(19, 87);
+            this.ListingLable.Name = "ListingLable";
+            this.ListingLable.Size = new System.Drawing.Size(0, 13);
+            this.ListingLable.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DATETIME",
+            "INT",
+            "VARCHAR(max)"});
+            this.comboBox1.Location = new System.Drawing.Point(204, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(136, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(204, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 37);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Добавить внешний ключ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(387, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Имя внешней таблицы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 356);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ColumnBox;
+        private System.Windows.Forms.TextBox ForeignBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox TableCreateBox;
+        private System.Windows.Forms.Button CreateTable;
+        private System.Windows.Forms.Button DeleteTable;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label ListingLable;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Lab9BD/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab9BD
+{
+    public partial class Form1 : Form
+    {
+        private List<string> listSqlCommands = new List<string>();
+        private string Connect = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connstr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            System.Console.WriteLine();
+            this.textBox1.Text = Connect;  
+        }
+
+
+        private void createDatabase(string sqlQuerry)
+        {
+            using(SqlConnection connection = new SqlConnection(Connect))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("create database " + sqlQuerry, connection);
+                listSqlCommands.Add(command.CommandText);
+                command.ExecuteNonQuery();
+            }
+
+        }
+        private void AddColumn(string name, string column, string type, string bdname)
+        {
+            using (SqlConnection connection = new SqlConnection(Connect))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("USE ["+ bdname + "] alter table " + name + " add " + column + " " + type, connection);
+                listSqlCommands.Add(command.CommandText);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void DeleteColumn(string name, string column, string bdname)
+        {
+            using( SqlConnection connection = new SqlConnection(Connect))
+            {
+                connection.Open ();
+                SqlCommand command = new SqlCommand("USE[" + bdname + "] Alter table " + name + " drop column " + column +";", connection);
+                listSqlCommands.Add(command.CommandText);
+                command.ExecuteNonQuery();
+            }
+        }
+        private void CreateTables(string bdname, string name)
+        {
+            using (SqlConnection connection = new SqlConnection(Connect))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("USE[" + bdname + "] create table " + name +"(" + name+ "_id  INT IDENTITY(1,1) PRIMARY KEY);", connection);
+                listSqlCommands.Add (command.CommandText);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void DeleteTables(string bdname, string name)
+        {
+            using(SqlConnection connection = new SqlConnection(Connect))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("USE[" + bdname + "] drop table" + name, connection);
+                listSqlCommands.Add(command.CommandText);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void Add_FOREIGNKEY(string bdname, string NamePrimary, string NameSecondary)
+        {
+            using(SqlConnection conn = new SqlConnection(Connect))
+            {
+                conn.Open();
+                SqlCommand commandADD = new SqlCommand("USE[" + bdname + "] alter table " + NamePrimary + " add " + NameSecondary+ "_id int;", conn);
+                listSqlCommands.Add(commandADD.CommandText);
+                commandADD.ExecuteNonQuery();
+                SqlCommand comandForeign = new SqlCommand("USE[" + bdname + "] alter table " + NamePrimary + " add foreign key (" + NameSecondary + "_id) references " + NameSecondary+"("+NameSecondary+"_id);", conn);
+                listSqlCommands.Add(comandForeign.CommandText);
+                comandForeign.ExecuteNonQuery();
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) => createDatabase(textBox1.Text);
+        private void button2_Click(object sender, EventArgs e) => AddColumn(NameBox.Text, ColumnBox.Text, comboBox1.Text, textBox1.Text);
+
+        private void button3_Click(object sender, EventArgs e) => DeleteColumn(NameBox.Text,ColumnBox.Text,textBox1.Text);
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("E:\\list.txt");
+                foreach (var item in listSqlCommands)
+                {
+                    sw.WriteLine(item);
+                    ListingLable.Text = ListingLable.Text + "\n" + item;
+
+                }
+                sw.Close();
+            }
+            catch (Exception ex) {
+                ListingLable.Text = ex.Message;
+            }
+        }
+
+        private void DeleteTable_Click(object sender, EventArgs e) => DeleteTables(textBox1.Text, TableCreateBox.Text);
+
+        private void CreateTable_Click(object sender, EventArgs e) => CreateTables(textBox1.Text, TableCreateBox.Text);
+
+        private void button5_Click(object sender, EventArgs e) => Add_FOREIGNKEY(textBox1.Text, NameBox.Text, ForeignBox.Text);
+    }
+}
